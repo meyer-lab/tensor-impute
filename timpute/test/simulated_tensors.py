e2641fac94f7eb4067861da4101f98c2ee9d321b
ehung <ethanhung11@gmail.com>
2022-12-05 16:26:39 -0800

    bugs

--- timpute/test/simulated_tensors.py
+++ timpute/test/simulated_tensors.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorly as tl
 from tensorly.cp_tensor import CPTensor
-from impute_helper import create_missingness
+from ..impute_helper import create_missingness
 
 def createCube(missing=0.0, size=(10, 20, 25)):
     s = np.random.gamma(2, 2, np.prod(size))
@@ -11,15 +11,20 @@ def createCube(missing=0.0, size=(10, 20, 25)):
     return tensor
 
 def createTensor(drop_perc=0.0, size=(10,10,10), rank=6, distribution="gamma", scale=1, *args):
+    r"""
+    Creates a random tensor following a set of possible distributions:
+    "gamma", "chisquare", "logistic", "exponential", "uniform", "normal"
+    """
     rng = np.random.default_rng(10)
 
     factors = []
     for i in size:
         if distribution == "gamma": factors.append(rng.gamma(args, scale=1, size=(i,rank)))
-        if distribution == "logistic": factors.append(np.random.logistic(size=(i,rank)))
-        if distribution == "exponential": factors.append(np.random.exponential((i,rank)))
-        if distribution == "uniform": factors.append(np.random.gamma[(i,rank)])
-        if distribution == "normal": factors.append(np.random.normal(size=(i,rank)))
+        if distribution == "chisquare": factors.append(rng.chisquare(args, size=(i,rank)))
+        if distribution == "logistic": factors.append(rng.logistic(size=(i,rank)))
+        if distribution == "exponential": factors.append(rng.exponential(size=(i,rank)))
+        if distribution == "uniform": factors.append(rng.uniform(size=(i,rank)))
+        if distribution == "normal": factors.append(rng.normal(size=(i,rank)))
 
     if scale != 1:
         for i in factors: i = i * scale
