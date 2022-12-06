991a6fb620ee95e80e1218b584a6304d6d5d58e6
ehung <ethanhung11@gmail.com>
2022-12-05 17:19:07 -0800

    bug fix

--- timpute/test/simulated_tensors.py
+++ timpute/test/simulated_tensors.py
@@ -10,7 +10,7 @@ def createCube(missing=0.0, size=(10, 20, 25)):
         tensor[np.random.rand(*size) < missing] = np.nan
     return tensor
 
-def createTensor(drop_perc=0.0, size=(10,10,10), rank=6, distribution="gamma", scale=1, *args):
+def createTensor(drop_perc=0.0, size=(10,10,10), rank=6, distribution="gamma", scale=1, par=1):
     r"""
     Creates a random tensor following a set of possible distributions:
     "gamma", "chisquare", "logistic", "exponential", "uniform", "normal"
@@ -19,8 +19,8 @@ def createTensor(drop_perc=0.0, size=(10,10,10), rank=6, distribution="gamma", s
 
     factors = []
     for i in size:
-        if distribution == "gamma": factors.append(rng.gamma(args, scale=1, size=(i,rank)))
-        if distribution == "chisquare": factors.append(rng.chisquare(args, size=(i,rank)))
+        if distribution == "gamma": factors.append(rng.gamma(par, scale=1, size=(i,rank)))
+        if distribution == "chisquare": factors.append(rng.chisquare(par, size=(i,rank)))
         if distribution == "logistic": factors.append(rng.logistic(size=(i,rank)))
         if distribution == "exponential": factors.append(rng.exponential(size=(i,rank)))
         if distribution == "uniform": factors.append(rng.uniform(size=(i,rank)))
