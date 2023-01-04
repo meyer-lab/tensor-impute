2d31ee661cab81967e7cd8a489772a10b9b8ffa5
eniohodzic <ehodzic@g.ucla.edu>
2023-01-04 12:48:17 -0800

    alpha input param got removed for some reason

--- timpute/cmtf.py
+++ timpute/cmtf.py
@@ -165,7 +165,7 @@ def cp_normalize(tFac):
     return tFac
 
 
-def perform_CP(tOrig, rank=6, tol=1e-6, n_iter_max=50, progress=False, callback=None):
+def perform_CP(tOrig, rank=6, alpha=None, tol=1e-6, n_iter_max=50, progress=False, callback=None):
     """ Perform CP decomposition. """
 
     if callback: callback.begin()
