447567606118a99c4aa6e6661878231de7ae0dd2
ehung <ethanhung11@gmail.com>
2023-05-03 13:51:19 -0700

    refix test cases (merge error)

--- timpute/test/test_impute.py
+++ timpute/test/test_impute.py
@@ -11,9 +11,6 @@ def test_impute_alter():
     test = Decomposition(alter()['Fc'].to_numpy())
     test.Q2X_chord(drop=30, repeat=1)
     assert max(test.chordQ2X[0]) >= .8
-    test.Q2X_entry(drop=9000, repeat=3, dropany=False, comparePCA=True)
-    assert len(test.entryQ2X) == len(test.entryQ2XPCA)
-    assert len(test.entryQ2X[0]) == len(test.entryQ2XPCA[0])
     test.Q2X_entry(drop=9000,repeat=3)
     assert max(test.entryQ2X[0]) >= .85
 
@@ -22,11 +19,8 @@ def test_impute_zohar():
     test = Decomposition(zohar().to_numpy())
     test.Q2X_chord(drop=5, repeat=1)
     assert max(test.chordQ2X[0]) >= .4
-    test.Q2X_entry(drop=3000, repeat=1, dropany=False, comparePCA=True)
-    assert len(test.entryQ2X) == len(test.entryQ2XPCA)
-    assert len(test.entryQ2X[0]) == len(test.entryQ2XPCA[0])
+    test.Q2X_entry(drop=3000, repeat=1)
     assert max(test.entryQ2X[0]) >= .5
-    assert max(test.entryQ2XPCA[0]) >= .4
 
 def test_impute_random():
     np.random.seed(5)
@@ -34,9 +28,6 @@ def test_impute_random():
     test = Decomposition(tl.cp_to_tensor(random_cp(shape, 10)))
     test.Q2X_chord(drop=10, repeat=1)
     assert max(test.chordQ2X[0]) >= .95
-    test.Q2X_entry(drop=100, repeat=1, dropany=False, comparePCA=True)
-    assert len(test.entryQ2X) == len(test.entryQ2XPCA)
-    assert len(test.entryQ2X[0]) == len(test.entryQ2XPCA[0])
     test.Q2X_entry(drop=100, repeat=1)
     assert max(test.entryQ2X[0]) >= .95
 
