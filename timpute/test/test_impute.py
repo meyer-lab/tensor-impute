7fdf2e8ee88f930c5f34893c808075f39993515b
ehung <ethanhung11@gmail.com>
2023-05-09 12:23:30 -0700

    minmax swap

--- timpute/test/test_impute.py
+++ timpute/test/test_impute.py
@@ -10,26 +10,26 @@ def test_impute_alter():
     np.random.seed(5)
     test = Decomposition(alter()['Fc'].to_numpy())
     test.Q2X_chord(drop=30, repeat=1)
-    assert max(test.chord_error[0]) <= 1-.8
+    assert min(test.chord_error[0]) <= 1-.8
     test.Q2X_entry(drop=9000,repeat=3)
-    assert max(test.entry_error[0]) <= 1-.85
+    assert min(test.entry_error[0]) <= 1-.85
 
 def test_impute_zohar():
     np.random.seed(5)
     test = Decomposition(zohar().to_numpy())
     test.Q2X_chord(drop=5, repeat=1)
-    assert max(test.chord_error[0]) <= 1-.4
+    assert min(test.chord_error[0]) <= 1-.4
     test.Q2X_entry(drop=3000, repeat=1)
-    assert max(test.entry_error[0]) <= 1-.5
+    assert min(test.entry_error[0]) <= 1-.5
 
 def test_impute_random():
     np.random.seed(5)
     shape = (10,10,10)
     test = Decomposition(tl.cp_to_tensor(random_cp(shape, 10)))
     test.Q2X_chord(drop=10, repeat=1)
-    assert max(test.chord_error[0]) <= 1-.95
+    assert min(test.chord_error[0]) <= 1-.95
     test.Q2X_entry(drop=100, repeat=1)
-    assert max(test.entry_error[0]) <= 1-.95
+    assert min(test.entry_error[0]) <= 1-.95
 
 def test_impute_noise_missing():
     np.random.seed(5)
@@ -41,6 +41,6 @@ def test_impute_noise_missing():
 
     test = Decomposition(tensor_2)
     test.Q2X_chord(drop=10, repeat=1)
-    assert max(test.chord_error[0]) <= 1-.95
+    assert min(test.chord_error[0]) <= 1-.95
     test.Q2X_entry(drop=100, repeat=1)
-    assert max(test.entry_error[0]) <= 1-.95
+    assert min(test.entry_error[0]) <= 1-.95
