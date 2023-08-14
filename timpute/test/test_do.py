02f3beaaeb2bbda482e36d079a8c52503064466e
ehung <ethanhung11@gmail.com>
2023-08-14 11:28:49 -0700

    missed one

--- timpute/test/test_do.py
+++ timpute/test/test_do.py
@@ -2,7 +2,7 @@ import numpy as np
 from ..method_DO import perform_DO
 from ..impute_helper import calcR2X
 from ..generateTensor import generateTensor
-from timpute.tracker import tracker
+from timpute.tracker import Tracker
 from timpute.decomposition import Decomposition
 from timpute.common import getSetup
 from timpute.plot import q2xentry
@@ -10,7 +10,7 @@ from timpute.plot import q2xentry
 def test_decomp_dopt(plot=False, method = perform_DO):
     tensor = generateTensor('known', shape = (10,10,10))
     decomp = Decomposition(tensor, max_rr = 6)
-    track = tracker(tensor)
+    track = Tracker(tensor)
 
     # average Q2X for components 1-6 (n=10) for a single test tensor with 10% imputation
     decomp.imputation(drop=0.1, repeat=10, callback=track, type='entry')
