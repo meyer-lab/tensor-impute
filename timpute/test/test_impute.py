cfcf730cfa1960a5f8895211dfdad7d9ac350099
ehung <ethanhung11@gmail.com>
2024-11-18 11:57:48 -0800

    test fixes

--- timpute/test/test_impute.py
+++ timpute/test/test_impute.py
@@ -3,22 +3,12 @@ import tensorly as tl
 from tensorly.random import random_cp
 from ..decomposition import Decomposition
 from ..impute_helper import *
-<<<<<<< HEAD
-from tensordata.alter import data as alter
-from tensordata.zohar import data as zohar
 from generateTensor import generateTensor
 
 def test_impute_alter():
     np.random.seed(5)
     tensor = generateTensor(type='alter')
     test = Decomposition(tensor)
-=======
-from ..generateTensor import *
-
-def test_impute_alter():
-    np.random.seed(5)
-    test = Decomposition(generateTensor('alter'))
->>>>>>> origin
     test.imputation(type='chord', drop=0.05, repeat=1)
     assert min(test.chord_total[0]) <= 1-.8
     test.imputation(type='entry', drop=0.05,repeat=3)
@@ -26,12 +16,8 @@ def test_impute_alter():
 
 def test_impute_zohar():
     np.random.seed(5)
-<<<<<<< HEAD
     tensor = generateTensor(type='zohar')
     test = Decomposition(tensor)
-=======
-    test = Decomposition(generateTensor('zohar'))
->>>>>>> origin
     test.imputation(type='chord', drop=0.05, repeat=1)
     assert min(test.chord_total[0]) <= 1-.4
     test.imputation(type='entry', drop=0.05, repeat=1)
